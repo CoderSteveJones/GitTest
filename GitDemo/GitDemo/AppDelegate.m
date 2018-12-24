d076d87641431c9786bc7a0a740d6efc3adeafba
SteveJones <benkong_ah@foxmail.com>
2018-12-24 14:00:25 +0800

    branch_2上的修改

--- GitDemo/GitDemo/AppDelegate.m
+++ GitDemo/GitDemo/AppDelegate.m
@@ -17,6 +17,9 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    
+    NSLog(@"branch_2修改");
     return YES;
 }
 
